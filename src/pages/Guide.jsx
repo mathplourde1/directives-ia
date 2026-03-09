fd9d7acb5b407c07107175ffc03a01f63868337c
mathieu.plourde.1 <mathieu.plourde.1@ulaval.ca>
2026-03-09 17:30:10 +0000

    File changes

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -672,7 +672,7 @@ export default function Guide() {
               <tbody>
                 {selections.map((s, i) =>
               <tr key={i}>
-                    <td>{s.etape}</td>
+                    <td><strong>{s.etape}</strong>{s.parenthese && <span style={{fontWeight:'normal'}}> ({s.parenthese})</span>}</td>
                     <td>{s.ia}</td>
                     <td style={{ whiteSpace: 'pre-wrap' }}>{s.justification}</td>
                     <td dangerouslySetInnerHTML={{ __html: formatExigences(s) }} />
