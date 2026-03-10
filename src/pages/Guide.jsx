73aede0737e8661232a9adb776b1e909ccf3f0ce
mathieu.plourde.1 <mathieu.plourde.1@ulaval.ca>
2026-03-10 14:54:23 +0000

    Manual code change

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -450,7 +450,7 @@ export default function Guide() {
       <div style={{ textAlign: 'left', marginBottom: 20 }}>
         Guide interactif proposé par le <a href="https://www.enseigner.ulaval.ca/a-propos" target="_blank" className="text-blue-800 underline">Service de soutien à l'enseignement</a>, développé par Mathieu Plourde, CC-By 2026 (version alpha 4)<br /><br />
         Ce guide interactif vous permet de rédiger en quelques étapes des directives complètes, claires et prêtes à transmettre aux étudiantes et étudiants concernant l'utilisation des SIA lors d'une évaluation. 
-      <ul>
+      <ul style="list-style-type: disc;">
       <li>Cochez les étapes concernées par l’évaluation</li>
       <li>Complétez les informations requises. Soyez concis et précis</li>
       <li>Soumettez le formulaire pour obtenir une synthèse des directives qui précisent les usages autorisés, les attentes pédagogiques et les balises à respecter</li>
