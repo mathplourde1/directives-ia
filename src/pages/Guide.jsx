773b5e7505b111ed2e236284b80c2907663713e6
mathieu.plourde.1 <mathieu.plourde.1@ulaval.ca>
2026-03-10 14:59:39 +0000

    Manual code change

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -446,7 +446,7 @@ export default function Guide() {
         .radio-disabled label, .radio-disabled input { color: #999; pointer-events: none; }
       `}</style>
 
-      <h1 className="mb-4 text-2xl font-semibold">Rédiger et personnaliser les directives d'utilisation des systèmes d'intelligence artificielle (SIA) pour une évaluation</h1>
+      <h1 className="mr-10 mb-4 ml-10 text-2xl font-semibold">Rédiger et personnaliser les directives d'utilisation des systèmes d'intelligence artificielle (SIA) pour une évaluation</h1>
       <div style={{ textAlign: 'left', marginBottom: 20 }} className="mr-5 ml-5">
         Guide interactif proposé par le <a href="https://www.enseigner.ulaval.ca/a-propos" target="_blank" className="text-blue-800 underline">Service de soutien à l'enseignement</a>, développé par Mathieu Plourde, CC-By 2026 (version alpha 4)<br /><br />
         Ce guide interactif vous permet de rédiger en quelques étapes des directives complètes, claires et prêtes à transmettre aux étudiantes et étudiants concernant l'utilisation des SIA lors d'une évaluation. 
